9b8720433b0d1d4247c937c4006b98b82f8cf999
Ivaylo Nikolov <ivailon@gmail.com>
2017-09-24 23:29:08 +0300

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Ivaylo Nikolov on 24/09/17.
+//  Copyright © 2017 Test. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
