a8947aa643578fe31f81c6e80f811535316715e0
Ivaylo Nikolov <ivailon@gmail.com>
2017-09-24 23:42:52 +0300

    Created view in order to demonstrate the issue

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -9,11 +9,21 @@
 import Cocoa
 
 class ViewController: NSViewController {
+	/// The first view
+	@IBOutlet weak var view1: NSView!
+	/// The second view
+	@IBOutlet weak var view2: NSView!
 
+	/// Called after the view controller’s view has been loaded into memory.
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		// Do any additional setup after loading the view.
+		
+		view1.wantsLayer = true
+		view1.layer?.backgroundColor = NSColor.orange.cgColor
+		view2.wantsLayer = true
+		view2.layer?.backgroundColor = NSColor.cyan.cgColor
 	}
 
 	override var representedObject: Any? {
@@ -21,7 +31,5 @@ class ViewController: NSViewController {
 		// Update the view, if already loaded.
 		}
 	}
-
-
 }
 
